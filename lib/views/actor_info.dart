f02955a396136769d3522194a7d24323b775f445
ruiteng2021 <tengrui13@gmail.com>
2021-12-15 19:38:05 -0500

    Modify actor info

--- lib/views/actor_info.dart
+++ lib/views/actor_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:movieversum/controllers/get_api_info.dart';
 
 class ActorInfo extends StatefulWidget {
@@ -30,6 +31,8 @@ class _ActorInfoState extends State<ActorInfo> {
     dispayData["popularity"] = result.popularity.toString();
     dispayData["profile_path"] = result.profilePath;
     dispayData["also_known_as"] = result.alsoKnownAs.toString();
+    dispayData["id"] = result.id.toString();
+    dispayData["name"] = result.name.toString();
     setState(() {
       // Your state change code goes here
     });
@@ -38,33 +41,120 @@ class _ActorInfoState extends State<ActorInfo> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        // colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.dstATop),
-        image: this.dispayData["profile_path"] == null
-            ? DecorationImage(
-                image: AssetImage("assets/images/defaultBackgroundImage.jpg"),
-                fit: BoxFit.cover,
-              )
-            : DecorationImage(
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                    'https://image.tmdb.org/t/p/w500/${this.dispayData["profile_path"]}'),
-              ),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Center(
-            // child: Card(
-            //   elevation: 10,
-            //   color: Colors.white,
-            //   child: Container(
-            //     width: 300,
-            //     height: 300,
-            //     alignment: Alignment.center,
-            //     child: Text('www.kindacode.com', style: TextStyle(fontSize: 24)),
-            //   ),
-            // ),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              // shape: BoxShape.rectangle,
+              image: this.dispayData["profile_path"] == null
+                  ? DecorationImage(
+                      image: AssetImage(
+                          "assets/images/defaultBackgroundImage.jpg"),
+                      fit: BoxFit.cover,
+                    )
+                  : DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          'https://image.tmdb.org/t/p/w500/${this.dispayData["profile_path"]}'),
+                    ),
             ),
+            child: Container(
+              decoration: BoxDecoration(color: Colors.black.withOpacity(0.7)),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 50.0),
+              child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  this.dispayData["profile_path"] == null
+                      ? Hero(
+                          tag: this.dispayData["id"].toString(),
+                          child: Container(
+                            width: 150.0,
+                            height: 150.0,
+                            decoration: new BoxDecoration(
+                                shape: BoxShape.circle, color: Colors.white70),
+                            child: Icon(
+                              FontAwesomeIcons.userAlt,
+                              color: Colors.brown,
+                            ),
+                          ),
+                        )
+                      : Hero(
+                          tag: this.dispayData["id"].toString(),
+                          child: Container(
+                            width: 150.0,
+                            height: 150.0,
+                            decoration: new BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: new DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(
+                                    'https://image.tmdb.org/t/p/w500/${this.dispayData["profile_path"]}'),
+                              ),
+                            ),
+                          ),
+                        ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    this.dispayData["name"].toString(),
+                    maxLines: 2,
+                    style: TextStyle(
+                        height: 1.4,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 9.0),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 10.0,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      'Release Date',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 10,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      'Revenue',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 10,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      'Run Time',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 10,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
